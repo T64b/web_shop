6d646996278299179906ccfaa5f51b5b6c02167b
Aleksandr Bronza <aleksandr.bronza@scoutbee.com>
2020-08-17 19:24:40 +0300

    lesson_16

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,6 @@
 TOKEN = '717786533:AAEjRWzCg2ZLFymlNHieHFg_YSd5bpO-q0w'
 DEFAULT_PHOTO_URL = 'https://ericson-lab.com/wp-content/uploads/2017/08' \
                    '/nophoto.png'
+GLOBAL_IP = '35.198.110.35'
+WEBHOOK_PREFIX = '/td'
+WEBHOOK_URL = f'https://{GLOBAL_IP}{WEBHOOK_PREFIX}'
